d3ff4270ca8627052a78c795f49028411957cb05
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-11-14 21:39:13 -0300

    Base Movement Script

--- Assets/Scripts/Player/MovementManager.cs
+++ Assets/Scripts/Player/MovementManager.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementManager : MonoBehaviour{
-    // Start is called before the first frame update
-    void Start(){
-        
+
+    InputManager PlayerInput { get => PlayerManager.instance.InputManager; }
+    Rigidbody PlayerRigidbody { get => PlayerManager.instance.PlayerRigidbody; }
+    GameObject MeshObject { get => PlayerManager.instance.MeshObject; }
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotationSpeed;
+
+    void FixedUpdate(){
+        float stickMag = PlayerInput.StickValue.SqrMagnitude();
+        if (PlayerInput.StickValue.x != 0) SelfRotation(PlayerInput.StickValue.x, stickMag, rotationSpeed);
+        if (PlayerInput.StickValue.y != 0) Movement(PlayerInput.StickValue.y, stickMag, moveSpeed);
     }
 
-    // Update is called once per frame
-    void Update(){
-        
+    void Movement(float uniDirection, float stickMag, float movementSpeed){
+        float finalDirectionValue = UniDirectionNormalizer(uniDirection);
+        finalDirectionValue = finalDirectionValue * (stickMag * movementSpeed) * Time.fixedDeltaTime;
+        PlayerRigidbody.MovePosition(transform.position + MeshObject.transform.forward * finalDirectionValue);
     }
+
+    void SelfRotation(float uniDirection, float stickMag, float rotationSpeed){
+        float finalDirectionValue = UniDirectionNormalizer(uniDirection);
+        finalDirectionValue = finalDirectionValue * stickMag * rotationSpeed;
+        Vector3 newRotation = new Vector3(0f,finalDirectionValue,0f); 
+        MeshObject.transform.Rotate(newRotation);
+    }
+
+    float UniDirectionNormalizer(float uniDirection) => uniDirection > 0 ? 1 : -1;
 }
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -7,10 +7,14 @@ public class PlayerManager : MonoBehaviour{
     
     [SerializeField] MovementManager movementManager;
     [SerializeField] InputManager inputManager;
+    [SerializeField] GameObject meshObject;
+    [SerializeField] Rigidbody playerRigidBody;
 
     public MovementManager MovementManager { get => movementManager; }
     public InputManager InputManager { get => inputManager; }
-
+    public GameObject MeshObject { get => meshObject; }
+    public Rigidbody PlayerRigidbody { get => playerRigidBody; }
+    
     void Awake() {
         SingletonCreation();    
     }
