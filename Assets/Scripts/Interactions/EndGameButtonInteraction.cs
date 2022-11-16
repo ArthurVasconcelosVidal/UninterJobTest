bdaab99e798fd9d2e053c01c968f91716471d0f9
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-11-16 16:38:49 -0300

    Finishing project

--- Assets/Scripts/Interactions/EndGameButtonInteraction.cs
+++ Assets/Scripts/Interactions/EndGameButtonInteraction.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 public class EndGameButtonInteraction : InteractionBase{
     NotificationManager NotificationManager { get => MainCanvasManager.instance.NotificationManager; }
+    AudioManager AudioManager { get => AudioManager.instance; }
     [SerializeField] float timeToRestart;
     protected override void ActionBehavior(){
         triggerCollider.enabled = false;
         SubscribeActions(false);
         RestartScene(timeToRestart);
-    }
+        AudioManager.PlayAudio(SoundList.InteractionCongrat);
+}
 
     async void RestartScene(float timeToRestart){
-        NotificationManager.ShowNotification($"Você finalizou o teste. O nivel irá reiniciar em {timeToRestart} segundos");
+        NotificationManager.ShowNotification($"Teste Completo. O nivel irá reiniciar em {timeToRestart} segundos", timeToRestart);
         const int MILLISECONDS_CONVERSION = 1000;
         await Task.Delay((int)(timeToRestart * MILLISECONDS_CONVERSION));
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
